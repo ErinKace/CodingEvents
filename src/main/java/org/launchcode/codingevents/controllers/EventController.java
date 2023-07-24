eb9b3af5c3b252a3f9197660a1a92f92428cd53e
ErinKace <erinkacerovskis@gmail.com>
2023-07-24 11:21:18 -0500

    Finished basic form

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,25 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        ArrayList<String> events = new ArrayList<>();
+        events.add("Some Event");
+        events.add("Other Event");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+    @GetMapping("create")
+    public String renderCreateEventForms() {
+        return "events/create";
+    }
+}
