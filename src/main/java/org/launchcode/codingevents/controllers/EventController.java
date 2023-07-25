66dfab4416d7d325ae908aa8c55c49eaaa56f475
ErinKace <erinkacerovskis@gmail.com>
2023-07-25 14:42:43 -0500

    Finished most of the excercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,14 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
-    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
     @GetMapping
     public String displayAllEvents(Model model) {
+        events.put("Summer of Code", "EXPLORING FUN TECHNOLOGY CONCEPTS IN OUR WEEK-LONG CODING CAMP.");
+        events.put("Coding & Cocktails","The Liquor – Intro to Javascript");
+        events.put("DevOps KC","We're well into summer here in KC, and so we're working on a nice way to cool off and have some relaxing conversations about DevOps and Tech.");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -25,7 +29,7 @@ public class EventController {
     }
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+        events.put(eventName, "No Description");
         return "redirect:";
     }
 }
